d56de896874fcc5d826e27c282d8840399957c99
Hendry Sathyan <hendry@insuredmine.com>
2025-07-07 04:32:21 +0530

    update

--- Recursion-Problems/Basics.js
+++ Recursion-Problems/Basics.js
@@ -1,11 +1,54 @@
 //problem to find the sum of digits
 
-
-const DigitSum = function (n){
-  if(n==0) return 0;
+// const DigitSum = function (n){
+//   if(n==0) return 0;
   
-  return (n%10) + DigitSum(Math.floor(n/10))
+//   return (n%10) + DigitSum(Math.floor(n/10))
     
+// }
+
+// console.log(DigitSum(999));
+
+
+//count zeros
+
+// const countZeeros = function (n){
+//   if(n==0) return 0;
+
+//   if(n%10==0)
+//   {
+//      return  1 + countZeeros(Math.floor(n/10))
+//   }
+//   else {
+  
+//   return 0 + countZeeros(Math.floor(n/10))
+//   }
+    
+// }
+
+// console.log(countZeeros(1000));
+
+
+//1to N
+
+const One2N = function (n)
+{
+
+
+  if(n==0) return 0;
+
+   One2N(n-1)
+        console.log(n);
+        
+    
+  
+
+
+
 }
 
-console.log(DigitSum(999));
+                    
+
+
+
+
